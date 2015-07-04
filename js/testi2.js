7e490c82823efdcd0f9ad4bf5f30fcddb57d7fda
Raims <raimscabrera@gmail.com>
2015-07-04 15:34:12 +0800

    Added testimonials, logo and dept logos.

---
+++ js/testi2.js
@@ -0,0 +1,14 @@
+
+var quotes=new Array()
+
+
+quotes[0]="<p>TomasinoWeb, for me is a very dynamic organization, yet with a simple goal, which is for Thomasians to get connected with information and reliable news they need during their stay in the University. Likewise, it also prepares its members to be professional after they graduate from UST. TomasinoWeb helped me learn how to go out of my comfort zone and experience an amazing college life.</p><footer>Jastyn Limon (Batch 5), Executive Secretary</footer>"
+
+quotes[1]="<p>Tomasinoweb will always be my second family in UST. This is where I realized my full potential as a student leader along with my fellow officers and members who are very passionate in making this organization what it is now. My college life would have never been 'extraordinary' without Tomweb!</p><footer>Marie Angeline Pagulayan (Batch 2), President (2011-2012)</footer>"
+
+quotes[2]="<p>TomWeb yung nagbigay kulay sa buhay kulehiyo ko. Kahit malayo ako sa pamilya ko hindi ko naramdaman na wala akong makakapitan habang nasa UST ako, dahil dito ako natuto maging isang mabuting estudyante, student leader, at tao. Wala siguro ako sa posisyon ko ngayon sa kumpanya kung hindi dahil sa TomWeb.</p><footer>Kit Valmadrid (Batch 1), External Vice President (2008-2009)</footer>"
+
+quotes[3]="<p>TomasinoWeb took me by surprise when it offered me the chance to step up and lead. I have to say that accepting that challenge was by far, the best decision I've ever made in my college life.</p><footer>Macy Sevilla (Batch 5), Vice President for Human Resources (2014-2015)</footer>"
+
+var whichquote=Math.floor(Math.random()*(quotes.length))
+document.write(quotes[whichquote])
