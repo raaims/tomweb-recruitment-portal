65e0f1b4893c665c08b284d39300774b01c796ca
agentklass200 <alecxisbanag@hotmail.com>
2015-07-18 19:51:05 +0800

    Add Dynamic Header and Footer
    Add header.php and footer.php
    Change index.html -> index.php

---
+++ index.php
@@ -0,0 +1,80 @@
+<?php
+$title = "Join | TomasinoWeb";
+include 'header.php';
+ ?>
+<div class="header container-fluid text-center">
+    <h1 class="">JOIN US</h1>
+    <p class="">Join TomasinoWeb. UST's official online student publication.
+        A university-wide organization composed of student volunteers who
+        express dedication to their passion through digital media.
+    </p>
+
+    <button type="button" class="btn btn-default">
+      Join Now!
+    </button>
+
+    <div class='row dept-pics collapsed'>
+      <div class="col-md-2"><img src="images/writing-illu-sm.png" alt="" class="center-block img-block"></div>
+      <div class="col-md-2"><img src="images/photog-illu-sm.png" alt="" class="center-block img-block"></div>
+      <div class="col-md-4"><img src="images/staff-illu-sm.png" alt="" class="center-block img-block"></div>
+      <div class="col-md-2"><img src="images/creatives-illu-sm.png" alt="" class="center-block img-block"></div>
+      <div class="col-md-2"><img src="images/webtech-illu-sm.png" alt="" class="center-block img-block"></div>
+    </div>
+
+    <div class='row'>
+      <div class="col-sm-2 writing"><img src="images/home-logo-writing.jpg" alt="" class="center-block img-responsive"></div>
+      <div class="col-sm-2 photog"><img src="images/home-logo-photog.jpg" alt="" class="center-block img-responsive"></div>
+      <div class="col-sm-4 staff"><img src="images/home-logo-staff.jpg" alt="" class="center-block img-responsive"></div>
+      <div class="col-sm-2 creatives"><img src="images/home-logo-ct.jpg" alt="" class="center-block img-responsive"></div>
+      <div class="col-sm-2 webtech"><img src="images/home-logo-wt.jpg" alt="" class="center-block img-responsive"></div>
+    </div>
+</div>
+
+<div class='values container-fluid'>
+  <div class='row'>
+    <div class="col-md-6 text-pad">
+      <h3>Versatility at its finest</h3>
+      <span>We live in the digital era and TomasinoWeb wants to explore its potential to revolutionize the way Thomasians access information. As an online student publication, TomasinoWeb explores multiple fields, namely: Writing, Creatives, Photography and Web Technologies. TomasinoWeb gathers enthusiasts of the said fields and assists them in developing their skills, not only in their declared expertise upon entering the organization, but in TomasinoWeb's other core fields as well.</span>
+    </div>
+    <div class="col-md-6 text-pad">
+      <h3>Lead telented individuals</h3>
+      <span>Just like the other university-wide organizations, one of TomasinoWeb's aims is to unravel its members' potential in the field of leadership. However, TomasinoWeb does not simply have ordinary people. TomasinoWeb is a league of extraordinary Thomasians. They are individuals with varying talents and fields of specialization, adding twists to the leadership development process.</span>
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="col-md-6 text-pad">
+      <h3>Social ground for passionate people</h3>
+      <span>We gather people with the same interests to work together for a common goal. Other than work, the organization also provides a social ground for its members to meet new colleagues and gain new friends, bringing the benefits of both work and friendship to each member.</span>
+    </div>
+    <div class="col-md-6 text-pad">
+      <h3>Imbued with Thomasian values</h3>
+      <span>As an organization of the University of Santo Tomas, TomasinoWeb ensures that it helps the University in instilling Thomasian values in its members. The organization ensures to be of service to the Thomasian community and aid in its involvement in the development of self-reliant communities.</span>
+    </div>
+  </div>
+</div>
+
+<!-- TESTIMONIALS BEGIN -->
+
+<div class='container-fluid '>
+  <div class='row'>
+    <h2 class="text-center">Testimonials</h2>
+    <div class="col-md-1"></div>
+    <div class="col-md-5 text-pad">
+      <blockquote>
+        <script type="text/javascript" src="js/testi.js"></script>
+      </blockquote>
+    </div>
+    <div class="col-md-5 text-pad">
+      <blockquote>
+        <script type="text/javascript" src="js/testi2.js"></script>
+      </blockquote>
+    </div>
+    <div class="col-md-1"></div>
+  </div>
+</div>
+
+<?php
+include 'footer.php';
+ ?>
+<!-- TESTIMONAILS END -->
