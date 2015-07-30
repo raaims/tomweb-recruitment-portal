ea4da1dd0371030464eea7cc45b3eabcadaafbb3
Raims <raimscabrera@gmail.com>
2015-07-30 21:49:14 +0800

    Done with the initail layout of about.php

--- about.php
+++ about.php
@@ -1,5 +1,5 @@
 <?php
-$title = "About | Join - TomasinoWeb";
+$title = "About Us | Join - TomasinoWeb";
 include 'header.php';
  ?>
       <div class="header about container-fluid text-center">
@@ -15,37 +15,39 @@ include 'header.php';
 
       <div class="container-fluid">
         <div class="row">
-          <div class="col-md-6">
-            <img class="img-responsive" src="images/about-1.jpg" alt="" />
+          <div class="col-md-6 about-img-1">
           </div>
 
-          <div class="col-md-6">
-            <h2>Founded to revolutionize the way information is accessed in UST</h2>
+          <div class="col-md-5">
+            <h2 class="text-right">Founded to revolutionize the way information is accessed in UST</h2>
+            <p class="text-right">TomasinoWeb was founded in 2007 by student volunteers mainly from the Faculty of Arts and Letters and Faculty of Engineering under the guidance of the Santo Tomas e-service Providers (STePS). It was the former STePS Director, Rev. Fr. Melchor Saria, who conceptualized the TomasinoWeb. In 2008, it has been recognized as a university-wide organization and online student publication of UST.</p>
           </div>
         </div>
 
         <div class="row light-gray-bg">
-          <div class="col-md-6 col-md-push-6">
-            <img class="img-responsive" src="images/about-1.jpg" alt="" />
+          <div class="about-img-2 col-md-6 col-md-push-6">
           </div>
 
-          <div class="col-md-6 col-md-pull-6">
+          <div class="col-md-5 col-md-pull-5">
             <h2>Our identity</h2>
+            <p>TomasinoWeb is UST's official online student publication. At the same time, it functions as a university-wide organization. As a student publication, TomasinoWeb publishes content relevant to the Thomasian community in the most accessible way possible: digitally. The members make sure to deliver quality content through the web, with quality graphical features, using web technologies to deviate from the traditional medium.</p>
           </div>
 
         </div>
 
         <div class="row">
-          <div class="col-md-6">
-            <img class="img-responsive" src="images/about-1.jpg" alt="" />
+          <div class="about-img-3 col-md-6">
           </div>
 
-          <div class="col-md-6">
-            <h2>How we do it</h2>
+          <div class="col-md-5">
+            <h2 class="text-right">How we do it</h2>
+            <p class="text-right">Members get to choose from TomasinoWeb's several member classification, be it a writer, photographer, digital artist, web developer or staff. Their training will depend on their chosen field, but they are still given the opportunity to learn from other fields if they wish to do so.</p>
           </div>
         </div>
+      </div>
 
-        <div class="row about-footer">
+      <div class="container-fluid about-footer">
+        <div class="container text-center">
           <h2>TomWebbers are not considered as mere members, they are family treasured even after their stay in the University.</h2>
           <button type="button" class="btn btn-default">
             Join Now!
