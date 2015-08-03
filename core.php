78375537a5c227cf7da92993b22d12f57ada2990
Raims <raimscabrera@gmail.com>
2015-08-03 12:02:48 +0800

    Initialized core.php

---
+++ core.php
@@ -0,0 +1,18 @@
+<?php
+$title = "The Core Group | Join - TomasinoWeb";
+include 'header.php';
+ ?>
+
+	<div class="header core container-fluid text-center">
+		<h1>THE CORE GROUP</h1>
+	</div>
+
+	<div class="black-bg container-fluid text-center">
+		<div class="content">
+			<span>TomasinoWeb is headed by officers collectively called The Core Group. It is composed of people who stepped-up to give back to the organization through directly managing its operations.</span>
+		</div>
+	</div>
+
+ <?php
+include 'footer.php';
+ ?>
