758578b40e0bba16b141a70bafe57f1fc76f0654
Raims <raimscabrera@gmail.com>
2015-08-18 13:43:11 +0800

    Update

--- core.php
+++ core.php
@@ -1,9 +1,8 @@
 <?php
 $title = "The Core Group | Join - TomasinoWeb";
 include 'header.php';
- ?>
-
-	<div class="header core container-fluid text-center">
+?>
+<div class="header core container-fluid text-center">
 		<h1>THE CORE GROUP</h1>
 	</div>
 
@@ -14,25 +13,25 @@ include 'header.php';
 	</div>
 
 	<div class="container text-center core-officers">
-		<div class="col-md-4"><span class="name">Raims Cabrera</span><br><span class="position">President</span></div>
-		<div class="col-md-4"><span class="name">Mariejo Gabuyo</span><br><span class="position">Executive Vice President</span></div>
-		<div class="col-md-4"><span class="name">Jastyn Limon</span><br><span class="position">Executive Secretary</span></div>
-		<div class="col-md-4"><span class="name">Zebadiah Canero</span><br><span class="position">Vice President for Human Resources</span></div>
-		<div class="col-md-4"><span class="name">Jose Chua Jr.</span><br><span class="position">Vice President for External Affairs</span></div>
-		<div class="col-md-4"><span class="name">Migz Jimenez</span><br><span class="position">Vice President for Finance</span></div>
-		<div class="col-md-4"><span class="name">Julia Ocampo</span><br><span class="position">Vice President for Community Development</span></div>
-		<div class="col-md-4"><span class="name">Nica Roque</span><br><span class="position">Vice President for Publicity and Communications</span></div>
-		<div class="col-md-4"><span class="name">Xave Gregorio</span><br><span class="position">Editor In Chief</span></div>
-		<div class="col-md-4"><span class="name">Rea Stevens</span><br><span class="position">Managing Editor</span></div>
-		<div class="col-md-4"><span class="name">Jem Sagcal</span><br><span class="position">Associate Editor</span></div>
-		<div class="col-md-4"><span class="name">Bria Cardenas</span><br><span class="position">Chief Photographer</span></div>
-		<div class="col-md-4"><span class="name">Joshua Lugti</span><br><span class="position">Asst. Chief Photographer</span></div>
-		<div class="col-md-4"><span class="name">Julius Renomeron Jr.</span><br><span class="position">Creative Director</span></div>
-		<div class="col-md-4"><span class="name">Humphrey Litan</span><br><span class="position">Asst. Creative Director</span></div>
-		<div class="col-md-4 col-md-offset-2"><span class="name">Alecxis Banag</span><br><span class="position">Webmaster</span></div>
-		<div class="col-md-4"><span class="name">Manrick Capotolan</span><br><span class="position">Asst. Webmaster</span></div>
+		<div class="col-md-4"><span class="name">Raims Cabrera</span><br><span class="position eb">President</span></div>
+		<div class="col-md-4"><span class="name">Mariejo Gabuyo</span><br><span class="position eb">Executive Vice President</span></div>
+		<div class="col-md-4"><span class="name">Jastyn Limon</span><br><span class="position eb">Executive Secretary</span></div>
+		<div class="col-md-4"><span class="name">Zebadiah Canero</span><br><span class="position eb">Vice President for Human Resources</span></div>
+		<div class="col-md-4"><span class="name">Jose Chua Jr.</span><br><span class="position eb">Vice President for External Affairs</span></div>
+		<div class="col-md-4"><span class="name">Migz Jimenez</span><br><span class="position eb">Vice President for Finance</span></div>
+		<div class="col-md-4"><span class="name">Julia Ocampo</span><br><span class="position eb">Vice President for Community Development</span></div>
+		<div class="col-md-4"><span class="name">Nica Roque</span><br><span class="position eb">Vice President for Publicity and Communications</span></div>
+		<div class="col-md-4"><span class="name">Xave Gregorio</span><br><span class="position wr">Editor In Chief</span></div>
+		<div class="col-md-4"><span class="name">Rea Stevens</span><br><span class="position wr">Managing Editor</span></div>
+		<div class="col-md-4"><span class="name">Jem Sagcal</span><br><span class="position wr">Associate Editor</span></div>
+		<div class="col-md-4"><span class="name">Bria Cardenas</span><br><span class="position pt">Chief Photographer</span></div>
+		<div class="col-md-4"><span class="name">Joshua Lugti</span><br><span class="position pt">Asst. Chief Photographer</span></div>
+		<div class="col-md-4"><span class="name">Julius Renomeron Jr.</span><br><span class="position cr">Creative Director</span></div>
+		<div class="col-md-4"><span class="name">Humphrey Litan</span><br><span class="position cr">Asst. Creative Director</span></div>
+		<div class="col-md-4 col-md-offset-2"><span class="name">Alecxis Banag</span><br><span class="position web">Webmaster</span></div>
+		<div class="col-md-4"><span class="name">Manrick Capotolan</span><br><span class="position web">Asst. Webmaster</span></div>
 	</div>
 
- <?php
+<?php
 include 'footer.php';
- ?>
+?>
